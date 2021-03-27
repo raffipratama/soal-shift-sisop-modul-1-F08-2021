60205315412f2e1bd2ac102222564fa10f222d60
Ajoedhi19 <74102918+Ajoedhi19@users.noreply.github.com>
2021-03-27 12:00:53 +0700

    Add files via upload

---
+++ 3a.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+for a in {1..23}
+do
+	wget -O "Koleksi_$a.jpg" https://loremflickr.com/320/240/kitten 2>> Foto.log | sort -k 23 -n -r
+done
+
+jml=23
+for (( a=1; a<=jml; a++ ))
+do
+	for (( b=a+1; b<=jml; b++ ))
+	do
+		cmp -s Koleksi_$a.jpg Koleksi_$b.jpg
+		compare=$?
+		if [ $compare -eq 0 ]
+		then
+			rm "Koleksi_$b.jpg"
+			let jml=$jml-1
+			let b=$b-1
+		fi
+	done
+done
+
+num=1
+for file in *.jpg;
+do 
+	if [ $num -le 9 ]
+	then
+		mv "$file" "Koleksi_0${num}.jpg"
+		let num=$num+1
+	else 
+		mv "$file" "Koleksi_${num}.jpg"
+		let num=$num+1
+	fi
+done
+
